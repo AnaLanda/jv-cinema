98a380ccc0bd9d868215d46227a5240d37578db7
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-10-07 13:51:15 +0300

    refactored Main class

--- src/main/java/com/cinema/Main.java
+++ src/main/java/com/cinema/Main.java
@@ -4,15 +4,11 @@ import com.cinema.exceptions.AuthenticationException;
 import com.cinema.lib.Injector;
 import com.cinema.model.User;
 import com.cinema.security.AutheticationService;
-import com.cinema.service.UserService;
 
 public class Main {
     private static Injector injector = Injector.getInstance("com.cinema");
-    private static final String DATE = "20201021";
-    private static final String DATE_TIME = "2020-10-21T10:15:30";
 
     public static void main(String[] args) throws AuthenticationException {
-        UserService userService = (UserService) injector.getInstance(UserService.class);
         AutheticationService autheticationService =
                 (AutheticationService) injector.getInstance(AutheticationService.class);
         User user1 = new User();
