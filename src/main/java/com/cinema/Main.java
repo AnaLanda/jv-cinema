c31a9994fc44ca4beb7204301013d77a4fa30328
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-10-07 13:50:20 +0300

    implemented dao and service for user

--- src/main/java/com/cinema/Main.java
+++ src/main/java/com/cinema/Main.java
@@ -1,59 +1,26 @@
 package com.cinema;
 
+import com.cinema.exceptions.AuthenticationException;
 import com.cinema.lib.Injector;
-import com.cinema.model.CinemaHall;
-import com.cinema.model.Movie;
-import com.cinema.model.MovieSession;
-import com.cinema.service.CinemaHallService;
-import com.cinema.service.MovieService;
-import com.cinema.service.MovieSessionService;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import com.cinema.model.User;
+import com.cinema.security.AutheticationService;
+import com.cinema.service.UserService;
 
 public class Main {
     private static Injector injector = Injector.getInstance("com.cinema");
     private static final String DATE = "20201021";
     private static final String DATE_TIME = "2020-10-21T10:15:30";
 
-    public static void main(String[] args) {
-        Movie movie = new Movie();
-        movie.setTitle("Ferris Bueller's Day Off");
-        movie.setDescription("A 1986 American teen comedy film starring Matthew Broderick as "
-                + "Ferris Bueller, a high-school slacker who skips school for a day in Chicago");
-        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
-        movieService.add(movie);
-        movieService.getAll().forEach(System.out::println);
-
-        CinemaHallService cinemaHallService =
-                (CinemaHallService)injector.getInstance(CinemaHallService.class);
-        CinemaHall hall1 = new CinemaHall();
-        hall1.setCapacity(30);
-        hall1.setDescription("A newly revamped VIP hall with a 3D screen and reclining armchairs");
-        cinemaHallService.add(hall1);
-        CinemaHall hall2 = new CinemaHall();
-        hall2.setCapacity(150);
-        hall2.setDescription("A standard hall");
-        cinemaHallService.add(hall2);
-        System.out.printf("All cinema halls: \n");
-        cinemaHallService.getAll().forEach(System.out::println);
-
-        MovieSession movieSession1 = new MovieSession();
-        movieSession1.setCinemaHall(hall1);
-        movieSession1.setShowTime(LocalDateTime.parse(DATE_TIME,
-                DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-        MovieSession movieSession2 = new MovieSession();
-        movieSession2.setMovie(movie);
-        movieSession2.setCinemaHall(hall2);
-        movieSession2.setShowTime(LocalDateTime.parse(DATE_TIME,
-                DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-        MovieSessionService movieSessionService =
-                (MovieSessionService) injector.getInstance(MovieSessionService.class);
-        movieSessionService.add(movieSession1);
-        movieSessionService.add(movieSession2);
-        System.out.printf("All available sessions: \n");
-        movieSessionService.findAvailableSessions(movie.getId(),
-                LocalDate.parse(DATE, DateTimeFormatter.BASIC_ISO_DATE))
-                .forEach(System.out::println);
+    public static void main(String[] args) throws AuthenticationException {
+        UserService userService = (UserService) injector.getInstance(UserService.class);
+        AutheticationService autheticationService =
+                (AutheticationService) injector.getInstance(AutheticationService.class);
+        User user1 = new User();
+        user1.setEmail("user@gmail.com");
+        user1.setPassword("password");
+        System.out.println("User1 has been registered: "
+                + autheticationService.register(user1.getEmail(), user1.getPassword()));
+        System.out.println("User1 has logged in: "
+                + autheticationService.login(user1.getEmail(), user1.getPassword()));
     }
 }
---
+++ src/main/java/com/cinema/dao/impl/UserDaoImpl.java
@@ -0,0 +1,51 @@
+package com.cinema.dao.impl;
+
+import com.cinema.dao.UserDao;
+import com.cinema.exceptions.DataProcessingException;
+import com.cinema.lib.Dao;
+import com.cinema.model.User;
+import com.cinema.util.HibernateUtil;
+import java.util.Optional;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User add(User user) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            return user;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Failed to add the user "
+                    + user + " to the DB.", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<User> query = session.createQuery("from User "
+                    + "where email = :email", User.class);
+            query.setParameter("email", email);
+            User user = query.uniqueResult();
+            return Optional.ofNullable(user);
+        } catch (Exception e) {
+            throw new DataProcessingException("Failed to find a user with the email "
+                    + email, e);
+        }
+    }
+}
